461b10a05b04bfae5dbb2d71b9d4362d00757193
David Meert <david@davidmeert.be>
2018-07-27 01:29:28 +0200

    add forgotten test

--- tests/Unit/Infrastructure/Services/TesseractOCRPaymentDocumentServiceTest.php
+++ tests/Unit/Infrastructure/Services/TesseractOCRPaymentDocumentServiceTest.php
@@ -6,12 +6,22 @@ namespace CompanyTools\Tests\Unit\Infrastructure\Services;
 
 use CompanyTools\Domain\Document\Bill;
 use CompanyTools\Domain\Document\Invoice;
+use CompanyTools\Domain\Document\ReferenceValue;
 use CompanyTools\Domain\Generic\ValueObject\File\FilePathValue;
 use CompanyTools\Infrastructure\Services\TesseractOCRPaymentDocumentService;
 use PHPUnit\Framework\TestCase;
 
 class TesseractOCRPaymentDocumentServiceTest extends TestCase
 {
+    public function testCreateReferenceValue()
+    {
+        $s = new TesseractOCRPaymentDocumentService();
+        $rv = $s->createReferenceValue('CompanyTools');
+
+        $this->assertInstanceOf(ReferenceValue::class, $rv);
+        $this->assertSame('CompanyTools', $rv->getValue());
+    }
+
     public function testInvoiceFromFile()
     {
         $fpv = $this->createMock(FilePathValue::class);
